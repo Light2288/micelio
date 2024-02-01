88760a5850a1a433d7da8a032e340180e716f50f
Light2288 <davide.aliti@gmail.com>
2024-02-01 13:40:47 +0100

    feature: initial map view
    add map view initial implementation
    add location manager to track user position
    add button to center again on user position

---
+++ Micelio/Models/LocationManager.swift
@@ -0,0 +1,8 @@
+//
+//  LocationManager.swift
+//  Micelio
+//
+//  Created by Davide Aliti on 31/01/24.
+//
+
+import Foundation
---
+++ Micelio/Views/Map/MapView.swift
@@ -0,0 +1,18 @@
+//
+//  MapView.swift
+//  Micelio
+//
+//  Created by Davide Aliti on 31/01/24.
+//
+
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MapView()
+}
